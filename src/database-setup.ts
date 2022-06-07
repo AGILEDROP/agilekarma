2c9921969dabae1956667053a393eae356822de2
David Pustoslemsek <pusti.david@gmail.com>
2022-06-07 16:13:21 +0200

    Updated database functions to arrow functions and typescript updates

--- src/database-setup.ts
+++ src/database-setup.ts
@@ -1,30 +1,24 @@
 'use strict';
+
+import { ConnectionConfig } from "mysql";
+
 require('dotenv').config();
 
 const mysql = require('mysql');
 
-const mysqlConfig = {
-  // eslint-disable-next-line no-process-env
+const mysqlConfig: ConnectionConfig = {
   host: process.env.DATABASE_HOST,
-  // eslint-disable-next-line no-process-env
-  port: process.env.DATABASE_PORT,
-  // eslint-disable-next-line no-process-env
+  port: process.env.DATABASE_PORT as unknown as number,
   user: process.env.DATABASE_USER,
-  // eslint-disable-next-line no-process-env
   password: process.env.DATABASE_PASSWORD,
-  // eslint-disable-next-line no-process-env
   database: process.env.DATABASE_NAME
 };
 console.log(mysqlConfig);
 
 /**
- *
  * Prepares database.
- *
- * @returns {Promise}
- *   The promise.
  */
-const prepareDatabase = async () => {
+const prepareDatabase = async (): Promise<any> => {
   await createUserTable();
   await createChannelTable();
   await createScoreTable();
@@ -34,12 +28,9 @@ const prepareDatabase = async () => {
 
 /**
  * Creates channel table.
- *
- * @returns {Promise}
- *   Returned promise.
  */
-function createChannelTable() {
-  return new Promise(function (resolve, reject) {
+const createChannelTable = (): Promise<any> => {
+  return new Promise((resolve, reject) => {
     const db = mysql.createConnection(mysqlConfig);
     const inserts = ['channel'];
     const str = 'CREATE TABLE IF NOT EXISTS ?? ' +
@@ -47,7 +38,7 @@ function createChannelTable() {
       '`channel_name` VARCHAR(255) NULL,' +
       'PRIMARY KEY (`channel_id`));';
     const query = mysql.format(str, inserts);
-    db.query(query, function (err: string, result: string) {
+    db.query(query, (err: string, result: string) => {
       if (err) {
         reject(err);
       } else {
@@ -59,17 +50,14 @@ function createChannelTable() {
 
 /**
  * Creates scores table.
- *
- * @returns {Promise}
- *   Returned promise.
  */
-function createScoreTable() {
-  return new Promise(function (resolve, reject) {
+const createScoreTable = (): Promise<any> => {
+  return new Promise((resolve, reject) => {
     const db = mysql.createConnection(mysqlConfig);
     const inserts = ['score'];
     const str = 'CREATE TABLE IF NOT EXISTS ?? (`score_id` VARCHAR(255) NOT NULL, `timestamp` DATETIME NOT NULL, `to_user_id` VARCHAR(255) NOT NULL, `from_user_id` VARCHAR(255) NOT NULL, `channel_id` VARCHAR(255) NOT NULL, `description` TEXT NULL, PRIMARY KEY (`score_id`), INDEX `fk_score_user_idx` (`to_user_id` ASC), INDEX `fk_score_user1_idx` (`from_user_id` ASC), INDEX `fk_score_channel1_idx` (`channel_id` ASC), CONSTRAINT `fk_score_user` FOREIGN KEY (`to_user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT `fk_score_user1` FOREIGN KEY (`from_user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT `fk_score_channel1` FOREIGN KEY (`channel_id`) REFERENCES `channel` (`channel_id`) ON DELETE NO ACTION ON UPDATE NO ACTION)';
     const query = mysql.format(str, inserts);
-    db.query(query, function (err: string, result: string) {
+    db.query(query, (err: string, result: string) => {
       if (err) {
         reject(err);
       } else {
@@ -81,12 +69,9 @@ function createScoreTable() {
 
 /**
  * Creates user table.
- *
- * @returns {Promise}
- *   Returned promise.
  */
-function createUserTable() {
-  return new Promise(function (resolve, reject) {
+const createUserTable = (): Promise<any> => {
+  return new Promise((resolve, reject) => {
     const db = mysql.createConnection(mysqlConfig);
     const inserts = ['user'];
     const str = 'CREATE TABLE IF NOT EXISTS ?? ' +
@@ -96,7 +81,7 @@ function createUserTable() {
       '`banned_until` DATETIME NULL,' +
       'PRIMARY KEY (`user_id`));';
     const query = mysql.format(str, inserts);
-    db.query(query, function (err: string, result: string) {
+    db.query(query, (err: string, result: string) => {
       if (err) {
         reject(err);
       } else {
@@ -105,7 +90,7 @@ function createUserTable() {
     });
   });
 }
-prepareDatabase().catch(function (reason) {
+prepareDatabase().catch((reason) => {
   console.log(reason);
 });
 
--- src/database-usernames.ts
+++ src/database-usernames.ts
@@ -1,11 +1,14 @@
 /* eslint-disable max-len */
 'use strict';
+
+import { ConnectionConfig } from "mysql";
+
 require('dotenv').config();
 const mysql = require('mysql');
 
-const mysqlConfig = {
+const mysqlConfig: ConnectionConfig = {
   host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
+  port: process.env.DATABASE_PORT as unknown as number,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME
@@ -14,31 +17,26 @@ console.log(mysqlConfig);
 
 /**
  * Adds usernames to database
- *
- * @returns {Promise}
- *   The promise.
  */
-const prepareDatabase = async () => {
+const prepareDatabase = async (): Promise<any> => {
   await addUsername();
   console.log('Usernames Added');
-  setTimeout((function () {
+  setTimeout((() => {
     return process.exit(22);
   }), 500);
 };
 
 /**
  * Creates user table.
- *
- * @returns {Promise}
- *   Returned promise.
  */
-function addUsername() {
-  return new Promise(function (resolve, reject) {
+
+const addUsername = (): Promise<any> => {
+  return new Promise((resolve, reject) => {
     const db = mysql.createConnection(mysqlConfig);
     const str = "UPDATE user SET user_username = LOWER( REPLACE( user_name, ' ', '' ) ) WHERE (user_username = '' OR user_username IS NULL)";
     const query = mysql.format(str);
 
-    db.query(query, function (err: string, result: string) {
+    db.query(query, (err: string, result: string) => {
       if (err) {
         reject(err);
       } else {
@@ -49,6 +47,8 @@ function addUsername() {
   });
 }
 
-prepareDatabase().catch(function (reason) {
+prepareDatabase().catch((reason) => {
   console.log(reason);
 });
+
+export { };
