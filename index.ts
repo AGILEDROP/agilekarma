038f5bd83af9b5f6fe6b0b1b1aaee44ffd1d0cb9
Benjamin Cizej <benjamin.cizej@agiledrop.com>
2022-11-25 12:01:24 +0100

    Disable console eslint rule

--- index.ts
+++ index.ts
@@ -36,7 +36,6 @@ const bootstrap = (options?: { express: Express; slack: any }) => {
   server.get('/userprofile', handleGet);
 
   return server.listen(PORT, () => {
-    // eslint-disable-next-line no-console
     console.log(`Listening on http://localhost:${PORT}`);
   });
 };
