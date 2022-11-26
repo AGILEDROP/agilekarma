a6e03fa6411418b7e1d42b98645b138ab8af9561
Benjamin Cizej <benjamin.cizej@agiledrop.com>
2022-11-26 21:12:43 +0100

    Refactor and cleanup database-setup.ts

--- src/database-setup.ts
+++ src/database-setup.ts
@@ -1,22 +1,88 @@
-'use strict';
+import { ConnectionConfig, format, createConnection } from 'mysql';
+import * as dotenv from 'dotenv';
 
-import { ConnectionConfig, format, createConnection } from "mysql";
+dotenv.config();
 
-require('dotenv').config();
+const {
+  DATABASE_HOST: host,
+  DATABASE_PORT: port = '3306',
+  DATABASE_USER: user,
+  DATABASE_PASSWORD: password,
+  DATABASE_NAME: database,
+} = process.env;
 
 const mysqlConfig: ConnectionConfig = {
-  host: process.env.DATABASE_HOST,
-  port: Number.parseInt(process.env.DATABASE_PORT, 10),
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME
+  host,
+  port: Number.parseInt(port, 10),
+  user,
+  password,
+  database,
 };
 console.log(mysqlConfig);
 
 /**
+ * Creates channel table.
+ */
+const createChannelTable = () => new Promise((resolve, reject) => {
+  const db = createConnection(mysqlConfig);
+  const inserts = ['channel'];
+  const str = 'CREATE TABLE IF NOT EXISTS ?? '
+      + '(`channel_id` VARCHAR(255) NOT NULL,'
+      + '`channel_name` VARCHAR(255) NULL,'
+      + 'PRIMARY KEY (`channel_id`));';
+  const query = format(str, inserts);
+  db.query(query, (err, result) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(result);
+    }
+  });
+});
+
+/**
+ * Creates scores table.
+ */
+const createScoreTable = () => new Promise((resolve, reject) => {
+  const db = createConnection(mysqlConfig);
+  const inserts = ['score'];
+  const str = 'CREATE TABLE IF NOT EXISTS ?? (`score_id` VARCHAR(255) NOT NULL, `timestamp` DATETIME NOT NULL, `to_user_id` VARCHAR(255) NOT NULL, `from_user_id` VARCHAR(255) NOT NULL, `channel_id` VARCHAR(255) NOT NULL, `description` TEXT NULL, PRIMARY KEY (`score_id`), INDEX `fk_score_user_idx` (`to_user_id` ASC), INDEX `fk_score_user1_idx` (`from_user_id` ASC), INDEX `fk_score_channel1_idx` (`channel_id` ASC), CONSTRAINT `fk_score_user` FOREIGN KEY (`to_user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT `fk_score_user1` FOREIGN KEY (`from_user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT `fk_score_channel1` FOREIGN KEY (`channel_id`) REFERENCES `channel` (`channel_id`) ON DELETE NO ACTION ON UPDATE NO ACTION)';
+  const query = format(str, inserts);
+  db.query(query, (err, result) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(result);
+    }
+  });
+});
+
+/**
+ * Creates user table.
+ */
+const createUserTable = () => new Promise((resolve, reject) => {
+  const db = createConnection(mysqlConfig);
+  const inserts = ['user'];
+  const str = 'CREATE TABLE IF NOT EXISTS ?? '
+      + '(`user_id` VARCHAR(255) NOT NULL,'
+      + '`user_name` VARCHAR(255) NULL,'
+      + '`user_username` VARCHAR(255) NULL,'
+      + '`banned_until` DATETIME NULL,'
+      + 'PRIMARY KEY (`user_id`));';
+  const query = format(str, inserts);
+  db.query(query, (err, result) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(result);
+    }
+  });
+});
+
+/**
  * Prepares database.
  */
-const prepareDatabase = async (): Promise<any> => {
+const prepareDatabase = async () => {
   await createUserTable();
   await createChannelTable();
   await createScoreTable();
@@ -24,70 +90,6 @@ const prepareDatabase = async (): Promise<any> => {
   console.log('Database prepared!');
 };
 
-/**
- * Creates channel table.
- */
-const createChannelTable = (): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    const db = createConnection(mysqlConfig);
-    const inserts = ['channel'];
-    const str = 'CREATE TABLE IF NOT EXISTS ?? ' +
-      '(`channel_id` VARCHAR(255) NOT NULL,' +
-      '`channel_name` VARCHAR(255) NULL,' +
-      'PRIMARY KEY (`channel_id`));';
-    const query = format(str, inserts);
-    db.query(query, (err: string, result: string) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-}
-
-/**
- * Creates scores table.
- */
-const createScoreTable = (): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    const db = createConnection(mysqlConfig);
-    const inserts = ['score'];
-    const str = 'CREATE TABLE IF NOT EXISTS ?? (`score_id` VARCHAR(255) NOT NULL, `timestamp` DATETIME NOT NULL, `to_user_id` VARCHAR(255) NOT NULL, `from_user_id` VARCHAR(255) NOT NULL, `channel_id` VARCHAR(255) NOT NULL, `description` TEXT NULL, PRIMARY KEY (`score_id`), INDEX `fk_score_user_idx` (`to_user_id` ASC), INDEX `fk_score_user1_idx` (`from_user_id` ASC), INDEX `fk_score_channel1_idx` (`channel_id` ASC), CONSTRAINT `fk_score_user` FOREIGN KEY (`to_user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT `fk_score_user1` FOREIGN KEY (`from_user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT `fk_score_channel1` FOREIGN KEY (`channel_id`) REFERENCES `channel` (`channel_id`) ON DELETE NO ACTION ON UPDATE NO ACTION)';
-    const query = format(str, inserts);
-    db.query(query, (err: string, result: string) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-}
-
-/**
- * Creates user table.
- */
-const createUserTable = (): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    const db = createConnection(mysqlConfig);
-    const inserts = ['user'];
-    const str = 'CREATE TABLE IF NOT EXISTS ?? ' +
-      '(`user_id` VARCHAR(255) NOT NULL,' +
-      '`user_name` VARCHAR(255) NULL,' +
-      '`user_username` VARCHAR(255) NULL,' +
-      '`banned_until` DATETIME NULL,' +
-      'PRIMARY KEY (`user_id`));';
-    const query = format(str, inserts);
-    db.query(query, (err: string, result: string) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-}
 prepareDatabase().catch((reason) => {
   console.log(reason);
 });
