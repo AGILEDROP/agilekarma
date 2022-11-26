d9479090c922542e507b7f898031413bb2317ddb
Benjamin Cizej <benjamin.cizej@agiledrop.com>
2022-11-26 18:20:53 +0100

    Further index.ts cleanup

--- index.ts
+++ index.ts
@@ -1,43 +1,40 @@
 import slackClient from '@slack/client';
 import bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
+import express from 'express';
 import { handleGet, handlePost } from './src/app.js';
 import { setSlackClient } from './src/slack.js';
 
 dotenv.config();
 
-const PORT = process.env.SCOREBOT_PORT || 80;
-const SLACK_OAUTH_ACCESS_TOKEN = process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN;
-const protocol = process.env.SCOREBOT_USE_SSL !== '1' ? 'http://' : 'https://';
-const frontendUrl = process.env.SCOREBOT_LEADERBOARD_URL;
-const FRONTEND_URL = protocol + frontendUrl;
+const {
+  SLACK_OAUTH_ACCESS_TOKEN: accessToken = '',
+  SCOREBOT_LEADERBOARD_URL: leaderboardUrl = '',
+  SCOREBOT_PORT: port = '80',
+  SCOREBOT_USE_SSL: useHttps = '0',
+} = process.env;
+const protocol = useHttps !== '1' ? 'http://' : 'https://';
+const frontendUrl = protocol + leaderboardUrl;
+const server = express();
+setSlackClient(new slackClient.WebClient(accessToken));
 
-const bootstrap = (options?: { express: Express; slack: any }) => {
-  // Allow alternative implementations of both Express and Slack to be passed in.
-  const server = options?.express || express();
-  setSlackClient(options?.slack || new slackClient.WebClient(SLACK_OAUTH_ACCESS_TOKEN));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', frontendUrl);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-  server.use((req: Request, res: Response, next: () => void) => {
-    res.header('Access-Control-Allow-Origin', FRONTEND_URL);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+server.use(bodyParser.json());
+server.enable('trust proxy');
+server.get('/', handleGet);
+server.post('/', handlePost);
 
-  server.use(bodyParser.json());
-  server.enable('trust proxy');
-  server.get('/', handleGet);
-  server.post('/', handlePost);
+server.get('/leaderboard', handleGet);
+server.get('/channels', handleGet);
+server.get('/fromusers', handleGet);
+server.get('/karmafeed', handleGet);
+server.get('/userprofile', handleGet);
 
-  server.get('/leaderboard', handleGet);
-  server.get('/channels', handleGet);
-  server.get('/fromusers', handleGet);
-  server.get('/karmafeed', handleGet);
-  server.get('/userprofile', handleGet);
-
-  return server.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
-  });
-};
-
-bootstrap();
+server.listen(Number.parseInt(port, 10), () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
